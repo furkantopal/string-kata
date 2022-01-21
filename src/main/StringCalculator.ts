c0d11f03eb4db1a27da1b9a347a4d6c8416da681
Rob Leach <rob.leach@smartfleetsolutions.com>
2022-01-21 15:53:53 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/StringCalculator.ts
+++ src/main/StringCalculator.ts
@@ -1,9 +1,13 @@
+
+//   //;\n1;2"
+//if start // take 3rd digit as delimeter
 export class StringCalculator {
   add(numbers: string): number {
-    numbers = numbers.replace("\n", ",");
-    const inputNumber: string[] = numbers.split(",");
+    const inputString: string[] = numbers.split("");
+     
+    console.log(inputString);
     let sum = 0;
-    inputNumber.forEach((element) => (sum = sum + +element));
+    inputString.forEach((element) => (sum = sum + +element));
     return sum;
   }
 }
